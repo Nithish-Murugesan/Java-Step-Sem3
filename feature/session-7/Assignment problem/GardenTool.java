4956b12353c1ca959cda0661dcf20dba0806265b
Nithish-Murugesan <nm8513@srmist.edu.in>
2026-09-25 16:06:09 +0000

    Create GardenTool.java

---
+++ feature/session-7/Assignment problem/GardenTool.java
@@ -0,0 +1,36 @@
+abstract class GardenTool {
+    public GardenTool() {
+    }
+
+    public abstract String use();
+}
+
+class CuttingTool extends GardenTool {
+    public CuttingTool() {
+        super();
+    }
+
+    public String use() {
+        return super.use() + "blade sharpened first";
+    }
+}
+
+class Pruner extends CuttingTool {
+    public Pruner() {
+        super();
+    }
+
+    public String use() {
+        return super.use() + ", then trimming branches precisely";
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        CuttingTool c = new CuttingTool();
+        Pruner p = new Pruner();
+
+        System.out.println(c.use());
+        System.out.println(p.use());
+    }
+}
